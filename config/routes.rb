d384dbf8d94ad0e626a38bdb86e1276b188f3397
Leah <leahnpetersen@gmail.com>
2016-04-26 16:14:32 -0700

    added links to market info

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # resources :markets, only: [:index, :show]
   root 'markets#index'
+  get '/markets' => 'markets#index', as: 'markets'
+
+  get '/markets/:id' => 'markets#show', as: 'market'
 end
