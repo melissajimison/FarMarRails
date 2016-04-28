4cc048ffc2c5da3b26ebfdc6615cb04d2ac6da58
Leah <leahnpetersen@gmail.com>
2016-04-28 13:35:58 -0700

    added buttons and paths to edit and delete products on vendor page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,20 @@
 class ProductsController < ApplicationController
+  def show
+
+  end
+
+  def update
+  end
+
+  def delete
+    redirect_to @product
+  end
+
+  def edit
+  end
+
+  def search
+    @product = Product.where(id: params[:id]).first
+    redirect_to @product
+  end
 end
